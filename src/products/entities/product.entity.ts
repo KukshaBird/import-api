2d24a7bdb82d7e81cd41dc7710bb4e85a9d70699
KukshaBird <samoilovartem1989@gmail.com>
2025-05-31 16:58:23 +0300

    Implement Product entity.

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,10 +1,91 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
-@Entity()
+export interface IDimensions {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+export interface IReview {
+  rating: number;
+  comment: string;
+  date: string;
+  reviewerName: string;
+  reviewerEmail: string;
+}
+
+export interface IMeta {
+  createdAt: string;
+  updatedAt: string;
+  barcode: string;
+  qrCode: string;
+}
+
+@Entity('products')
 export class Product {
-  @PrimaryGeneratedColumn()
+  @PrimaryColumn('int')
   id: number;
 
   @Column()
-  name: string;
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @Column()
+  category: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  price: number;
+
+  @Column('decimal', { precision: 5, scale: 2 })
+  discountPercentage: number;
+
+  @Column('decimal', { precision: 3, scale: 2 })
+  rating: number;
+
+  @Column('int')
+  stock: number;
+
+  @Column('simple-array')
+  tags: string[];
+
+  @Column()
+  brand: string;
+
+  @Column()
+  sku: string;
+
+  @Column('int')
+  weight: number;
+
+  @Column('json')
+  dimensions: IDimensions;
+
+  @Column()
+  warrantyInformation: string;
+
+  @Column()
+  shippingInformation: string;
+
+  @Column()
+  availabilityStatus: string;
+
+  @Column('json')
+  reviews: Array<IReview>;
+
+  @Column()
+  returnPolicy: string;
+
+  @Column('int')
+  minimumOrderQuantity: number;
+
+  @Column('json')
+  meta: IMeta;
+
+  @Column('simple-array')
+  images: string[];
+
+  @Column()
+  thumbnail: string;
 }
