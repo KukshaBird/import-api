45a680c7efc04c7c64fb994ed9914c957fd462b1
KukshaBird <samoilovartem1989@gmail.com>
2025-05-31 17:37:31 +0300

    Implement base product service.

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './entities/product.entity';
+import { CreateProductDto } from './dtos/create-product.dto';
+import { UpdateProductDto } from './dtos/update-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  create(createProductDto: CreateProductDto) {
+    const product = this.productRepository.create(createProductDto);
+
+    return this.productRepository.save(product);
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.findOne({ where: { id } });
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    this.productRepository.merge(product, updateProductDto);
+    return this.productRepository.save(product);
+  }
+
+  findAll() {
+    return this.productRepository.find();
+  }
+}
