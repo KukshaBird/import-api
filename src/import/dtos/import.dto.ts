722705af116ae62ef8284f7fcaed633461d93443
KukshaBird <samoilovartem1989@gmail.com>
2025-06-03 16:08:45 +0300

    Added Swagger.

--- src/import/dtos/import.dto.ts
+++ src/import/dtos/import.dto.ts
@@ -1,5 +1,6 @@
 import { IsArray, IsEnum, IsOptional } from 'class-validator';
 import { Expose, Exclude, Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export enum ImportStatus {
   PENDING = 'pending',
@@ -9,9 +10,18 @@ export enum ImportStatus {
 }
 
 export class ImportDto {
+  @ApiProperty({
+    description: 'Unique identifier of the import',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
   @Expose()
   id: string;
 
+  @ApiPropertyOptional({
+    enum: ImportStatus,
+    default: ImportStatus.PENDING,
+    description: 'Current status of the import process',
+  })
   @Expose()
   @IsEnum(ImportStatus)
   @IsOptional()
@@ -20,6 +30,12 @@ export class ImportDto {
   @Exclude()
   resource: string;
 
+  @ApiPropertyOptional({
+    type: [Number],
+    description: 'Array of product IDs included in the import',
+    default: [],
+    example: [1, 2, 3],
+  })
   @Expose()
   @Type(() => Number)
   @IsArray()
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const { PORT, VERSION } = process.env;
 
@@ -12,6 +13,17 @@ async function bootstrap() {
     defaultVersion: VERSION ?? '1',
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Swagger
+  const options = new DocumentBuilder()
+    .setTitle('Import API')
+    .setDescription('The import products API description')
+    .setVersion('1.0')
+    // .addTag('base')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT ?? 3000);
 }
 void bootstrap();
